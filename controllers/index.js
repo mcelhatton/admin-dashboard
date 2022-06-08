d8d53961701a70f65147ad0a62eeb87e2b961e99
mcelhatton <colinm@mobiledealerdata.com>
2022-06-08 16:39:08 -0500

    update to readme file

--- controllers/index.js
+++ controllers/index.js
@@ -8,7 +8,7 @@ const chatRoutes = require('./chat-routes.js');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/chat', chatRoutes);
+router.use('/dashboard/chat', chatRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
